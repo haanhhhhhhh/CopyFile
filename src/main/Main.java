3a248f2089c5d05201dbfb2a489b2ce36a09da00
naicon <naicon@localhost>
2023-10-31 15:16:26 +0700

    CopyFile

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+
+package main;
+
+import controller.CopyFileProgram;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        new CopyFileProgram().run();
+    }
+    
+}
